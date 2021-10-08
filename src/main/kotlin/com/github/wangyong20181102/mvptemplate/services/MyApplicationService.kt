2773e98099a3233758c1404ea896d25e3ebeb4eb
GitHub Action <action@github.com>
2021-10-08 07:06:35 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/wangyong20181102/mvptemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.wangyong20181102.mvptemplate.services
+
+import com.github.wangyong20181102.mvptemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/wangyong20181102/mvptemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wangyong20181102/mvptemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.wangyong20181102.mvptemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wangyong20181102.mvptemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
